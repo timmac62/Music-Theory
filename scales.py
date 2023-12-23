0a308cd1a26fbd1c3b0ce83fd7bd26b1b2ee642c
timmac62 <tmac62@me.com>
2023-12-23 13:50:50 -0500

    Updated UI for Theory and Scales

--- scales.py
+++ scales.py
@@ -10,6 +10,7 @@ from music_theory import interval_names, doremi, keys, scales
 
 class ScalesApp(App):
     CSS_PATH = "scales.tcss"
+    TITLE = "Musical Scales"
     BINDINGS = [
         ("k", "key_change", "Change Key"),
         ("q", "quit", "Quit"),
@@ -20,15 +21,14 @@ class ScalesApp(App):
         yield Header()
         with Container(id="app-grid"):
             with Vertical(id="key-pane"):
-                # BORDER_TITLE = "Key"
                 yield Static(f"Key of: {self.scale_key}", id="key")
                 options = [(key, i) for i, key in enumerate(keys)]
                 yield Select(options, prompt="Select Key:")
             with Horizontal(id="scale-pane"):
-                for interval in interval_names:
-                    yield Static(f"{interval}")
-                for dores in doremi:
-                    yield Static(f"{dores}")
+                # for interval in interval_names:
+                #     yield Static(f"{interval}")
+                # for tone in doremi:
+                #     yield Static(f"{tone}")
                 yield Static("C", id="note0", classes="notes")
                 yield Static("D", id="note1", classes="notes")
                 yield Static("E", id="note2", classes="notes")
@@ -38,9 +38,12 @@ class ScalesApp(App):
                 yield Static("B", id="note6", classes="notes")
                 yield Static("C", id="note7", classes="notes")
             with Container(id="theory-pane"):
-                yield Static("This")
-                yield Static("panel")
-                yield Static("is")
+                for note_number in range(8):
+                    yield Static(f"{note_number}")
+                for interval in interval_names:
+                    yield Static(f"{interval}")
+                for tone in doremi:
+                    yield Static(f"{tone}")
             with Container(id="variants-pane"):
                 yield Static("Major Pentatonic")
                 yield Static("1 2 3 5 6")
@@ -49,7 +52,16 @@ class ScalesApp(App):
                 yield Static("1 ♭3 4 5 ♭7")
                 yield Static("C D E♭ G B♭", id="minorpentatonic")
         yield Footer()
-        # self.update_note_widgets()
+
+    def on_mount(self):
+        key_pane = self.query_one("#key-pane")
+        key_pane.border_title = "Key"
+        scale_pane = self.query_one("#scale-pane")
+        scale_pane.border_title = "Scale"
+        theory_pane = self.query_one("#theory-pane")
+        theory_pane.border_title = "Theory"
+        variant_pane = self.query_one("#variants-pane")
+        variant_pane.border_title = "Variants"
 
     def flatten_note(self, note_to_flatten):
         if "♯" in note_to_flatten:
@@ -76,16 +88,9 @@ class ScalesApp(App):
         self.query_one("#majorpentatonic").update(f"{mp1} {mp2} {mp3} {mp4} {mp5}")
         mp1 = scales[self.scale_key][0]
         mp2 = self.flatten_note(scales[self.scale_key][2])  # flatten the 3rd
-        # if "♯" in mp2:
-        #     mp2new = mp2.replace("♯", "")
-        # else:
-        #     mp2new = "♭" + mp2
         mp3 = scales[self.scale_key][3]
         mp4 = scales[self.scale_key][4]
         mp5 = self.flatten_note(scales[self.scale_key][6])  # flatten the 7th
-        # if "♯" in mp5:
-        #     mp5new = mp5.replace("♯", "")
-        # else:
         #     mp5new = "♭" + mp5
         self.query_one("#minorpentatonic").update(f"{mp1} {mp2} {mp3} {mp4} {mp5}")
 
